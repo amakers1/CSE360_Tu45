e6898b68d722cc82edfbdf7d9da2e3b4e9a283c9
alexalvarado03 <128116840+alexalvarado03@users.noreply.github.com>
2024-04-02 23:21:24 -0700

    Add files via upload
    updated basePageViewBuilder(can be used to start building nurse view and doctor view) and patientBaseView so now some GUI is functional

--- BasePageViewBuilder.java
+++ BasePageViewBuilder.java
@@ -1,10 +1,13 @@
 package application;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
 public class BasePageViewBuilder extends HBox{
 	
@@ -22,19 +25,19 @@ public class BasePageViewBuilder extends HBox{
 	
 	//String:
 	protected String userStatus;
+
 	
-	
-	public BasePageViewBuilder() {
+	public BasePageViewBuilder(String userStatus) {
 		logoutButton = new Button("Logout");
 		messagePortalButton = new Button("Message Portal");
 		
-		userStatus = "Patient";
+		this.userStatus = userStatus;
 		mainPane = new BorderPane();
-		mainPane.setStyle("-fx-background-color: #5E0202;"); //color is #5E0202
-		
+		mainPane.setStyle("-fx-background-color: #5E0202;" + "-fx-border-color: #FFC627;" + "-fx-border-width: 3;\n");//color is #5E0202
+
 		buildPage();
 
-		this.setPadding(new Insets(10,10,10,10));
+		//this.setPadding(new Insets(10,10,10,10));
 		this.getChildren().add(mainPane);
 	}
 	
@@ -42,7 +45,7 @@ public class BasePageViewBuilder extends HBox{
 	protected void buildPage() {
 		try {
 			if(userStatus == "Patient") {
-				PatientBaseViewPage patientViewPage = new PatientBaseViewPage();
+				PatientBaseViewPage patientViewPage = new PatientBaseViewPage(userStatus);
 				mainPane.getChildren().add(patientViewPage);
 			}else if(userStatus == "Nurse") {
 				//setNurseScreen;
--- PatientBaseViewPage.java
+++ PatientBaseViewPage.java
@@ -1,13 +1,21 @@
 package application;
 
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class PatientBaseViewPage extends BasePageViewBuilder{
 	
@@ -15,19 +23,22 @@ public class PatientBaseViewPage extends BasePageViewBuilder{
 	//***********************
 	
 	//VBoxs:
-	protected VBox topPane, leftPane, rightPane, centerPane;
+	protected VBox rightPane, centerPane, appointmentSummaryPane;
+	
+	//HBoxs:
+	protected HBox topPane, leftPane;
 	
 	//Labels:
-	protected Label firstNameLabel, lastNameLabel, dateOfBirthLabel, emailLabel, phoneNumberLabel;
+	protected Label firstNameLabel, lastNameLabel, dateOfBirthLabel, emailLabel, phoneNumberLabel, errorLabel;
 	
 	//TextFields:
 	protected TextField firstNameTextField, lastNameTextField, dateOfBirthTextField, emailTextField, phoneNumberTextField;
 	
 	//Buttons:
-	protected Button updateButton;
+	protected Button updateButton, contactInfoButton, appointmentSummaryButton;
 	
 	//StackPanes:
-	protected StackPane contactInfoPane, appointmentSummaryPane;
+	protected StackPane contactInfoPane;
 	
 	//Scenes:
 	protected Scene contactInfoScene, appointmentSummaryScene;
@@ -36,26 +47,56 @@ public class PatientBaseViewPage extends BasePageViewBuilder{
 	protected TextArea node1, node2, node3;
 	
 	//GridPane:
-	protected GridPane contactGrid;
+	protected GridPane contactGrid, topPaneGrid;
 	
-	//test
+	//Image:
+	protected Image image;
+	protected ImageView imageView;
 	
-	public PatientBaseViewPage() {
-		super();
+	//Strings:
+	protected String firstName, lastName, email;
+	
+	//Ints:
+	protected int dateOfBirth, phoneNumber;
+	
+	public PatientBaseViewPage(String userStatus) {
+		super(userStatus);
+		
 	}
+	
 	@Override
 	public void buildPage() {
 		//Intitalizing Variables:
 		
+		//Images:
+		image = new Image("ASU_Hostpial_Logo_2.png");
+		imageView  = new ImageView(image);
+		imageView.setFitHeight(350);
+		imageView.setFitWidth(500);
+		
+		
 		//Labels:
 		firstNameLabel = new Label("First Name");
+		firstNameLabel.setStyle("-fx-font-family: Times New Roman; -fx-font-size: 25; -fx-text-fill: #FFC627");
+		firstNameLabel.setEllipsisString("First Name");
 		lastNameLabel = new Label("Last Name");
+		lastNameLabel.setStyle("-fx-font-family: Times New Roman; -fx-font-size: 25; -fx-text-fill: #FFC627");
 		dateOfBirthLabel = new Label("Date of Birth");
+		dateOfBirthLabel.setEllipsisString("Date of Birth");
+		dateOfBirthLabel.setStyle("-fx-font-family: Times New Roman; -fx-font-size: 25; -fx-text-fill: #FFC627");
 		emailLabel = new Label("Email");
+		emailLabel.setStyle("-fx-font-family: Times New Roman; -fx-font-size: 25; -fx-text-fill: #FFC627");
 		phoneNumberLabel = new Label("Phone Number");
-		
+		phoneNumberLabel.setEllipsisString("Phone Number");
+		phoneNumberLabel.setStyle("-fx-font-family: Times New Roman; -fx-font-size: 25; -fx-text-fill: #FFC627");
+		welcomeLabel = new Label("Welcome, PatientName");
+		welcomeLabel.setStyle("-fx-font-family: Times New Roman; -fx-font-size: 25; -fx-text-fill: #FFC627");
+		errorLabel = new Label("");
+		errorLabel.setStyle("-fx-font-family: Times New Roman; -fx-font-size: 15; -fx-text-fill: #FFC627");
+
 		//TextFields:
 		firstNameTextField = new TextField();
+		firstNameTextField.setMinWidth(200);
 		lastNameTextField = new TextField();
 		dateOfBirthTextField = new TextField();
 		emailTextField = new TextField();
@@ -63,29 +104,105 @@ public class PatientBaseViewPage extends BasePageViewBuilder{
 		
 		//Buttons:
 		updateButton = new Button("Update");
+		updateButton.setEllipsisString("Update");
+		contactInfoButton =  new Button("Contact Information");
+		appointmentSummaryButton = new Button("Appointment Summary");
 		
 		//TextAreas:
 		node1 = new TextArea();
+		node1.setMinWidth(607);
+		node1.setMinHeight(151);
 		node2 = new TextArea();
+		node2.setMinWidth(603);
+		node2.setMinHeight(150);
 		node3 = new TextArea();
+		node3.setMinWidth(603);
+		node3.setMinHeight(150);
 		
 		//StackPanes:
 		contactInfoPane = new StackPane();
-		appointmentSummaryPane = new StackPane();
 		
 		//GridPanes:
 		contactGrid = new GridPane();
-		contactGrid.addColumn(0, firstNameLabel, lastNameLabel, dateOfBirthLabel, emailLabel, phoneNumberLabel);
-		contactGrid.addColumn(1, firstNameTextField, lastNameTextField, dateOfBirthTextField, emailTextField, phoneNumberTextField);
+		contactGrid.setVgap(75);
+		contactGrid.setHgap(250);
+		contactGrid.addColumn(0, firstNameLabel, lastNameLabel, dateOfBirthLabel, emailLabel, phoneNumberLabel, errorLabel);
+		contactGrid.addColumn(1, firstNameTextField, lastNameTextField, dateOfBirthTextField, emailTextField, phoneNumberTextField, updateButton);
+		topPaneGrid = new GridPane();
+		topPaneGrid.setVgap(50);
+		topPaneGrid.setHgap(250);
+		topPaneGrid.addRow(0, welcomeLabel,messagePortalButton, logoutButton);
+		topPaneGrid.addRow(1, contactInfoButton,new Label("") , appointmentSummaryButton);
+		
+		//HBoxs:
+		topPane = new HBox(150);
+		topPane.getChildren().addAll(topPaneGrid);
 		
 		//VBoxs:
-		topPane = new VBox(50);
+		appointmentSummaryPane = new VBox(50);
+		appointmentSummaryPane.getChildren().addAll(node1, node2, node3);
 		centerPane = new VBox(50);
+		
+		leftPane = new HBox();
+		leftPane.getChildren().add(new Label(""));
+		rightPane = new VBox();
+		rightPane.getChildren().add(imageView);
 		centerPane.getChildren().addAll(contactGrid);
 		
+		topPane.setPadding(new Insets(30,100,57,100));
+		contactInfoPane.setPadding(new Insets(30,30,120,30));
+		leftPane.setPadding(new Insets(100,100,100,10));
+		rightPane.setPadding(new Insets(100,57,100,100));
+		contactInfoPane.getChildren().add(centerPane);
 		mainPane.setTop(topPane);
-		contactInfoPane.getChildren().addAll(centerPane);
 		mainPane.setCenter(contactInfoPane);
+		mainPane.setLeft(leftPane);
+		mainPane.setRight(rightPane);
+		
+		updateButton.setOnAction(e -> getData());
+		appointmentSummaryButton.setOnAction(e -> changeViewPane());
+		contactInfoButton.setOnAction(e -> changeViewPane());
+		
+	}
+	
+	private void getData() {
+		try {
+			if(firstNameTextField.getText().isBlank() || lastNameTextField.getText().isBlank() || emailTextField.getText().isBlank() || 
+					dateOfBirthTextField.getText().isBlank() || phoneNumberTextField.getText().isBlank()) {
+				
+				throw new Exception();
+			}
+			firstName = firstNameTextField.getText();
+			lastName = lastNameTextField.getText();
+			email = emailTextField.getText();
+			dateOfBirth = Integer.parseInt(dateOfBirthTextField.getText());
+			phoneNumber = Integer.parseInt(phoneNumberTextField.getText());
+			
+			System.out.print(firstName + lastName + email + dateOfBirth + phoneNumber);
+			
+			errorLabel.setText("");
+		}catch(NumberFormatException e) {
+			System.out.print(e);
+			errorLabel.setText("Date of Birth and Phone Number need to be numbers!");
+			errorLabel.setEllipsisString("Date of Birth and Phone Number need\n to be numbers!");
+			errorLabel.setStyle("-fx-font-family: Times New Roman; -fx-font-size: 15; -fx-text-fill: #FFC627");
+			
+			
+		}catch(Exception e) {
+			System.out.print(e);
+			errorLabel.setText("At least one field is empty!");
+			errorLabel.setEllipsisString("At least one field is empty!");
+		}
+	}
+	
+	private void changeViewPane() {
+		
+		if(contactInfoPane.getChildren().contains(contactGrid)) {
+			contactInfoPane.getChildren().setAll(appointmentSummaryPane);
+		}else {
+			contactInfoPane.getChildren().setAll(contactGrid);
+		}
+		
 	}
 
 }
